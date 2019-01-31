cf4d2574cd130403f3b6ac9676e21dde64d8d777
TeoGore <matteo.goretti01@universitadipavia.it>
2019-01-31 09:10:01 +0100

    done some performance testing, and continued  query analyzer

--- query_string_analyzer.py
+++ query_string_analyzer.py
@@ -1,11 +1,11 @@
-import os, pickle, urllib.parse
+import os, pickle, urllib.parse, time
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB, ComplementNB
-from sklearn.neighbors import NearestNeighbors, RadiusNeighborsClassifier, KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC, NuSVC
 
@@ -15,6 +15,10 @@ from sklearn import metrics
 N = 3       # Ngram value
 RANDOM_STATE = 123
 
+
+def time_stamp(init_time):
+    end_time = time.time()
+    print(f'\n*********\tExecution time: {end_time-init_time}\t*********\n')
 
 def save_pickle(variable, filename):
     save_file = open(filename, 'wb')
@@ -58,6 +62,8 @@ def validate_model(model, x_validation, y_validation, model_name):
     print(f'F1-Score: \t{metrics.f1_score(y_validation, y_predicted)*100:10.2f}')
     print(f'AUC: \t\t{auc*100:10.2f}')
 
+
+start_time = time.time()
 
 # Creating inputs (X)
 good_queries = load_file('dataset/kdn_url_queries/goodqueries.txt')
@@ -108,6 +114,10 @@ print(f'Y_TEST: \t\t{len(y_test)}')
 # quindi quelle fatte in load_file(), n_gram_tokenizer() e nel tfidf vectorizer.fit_transform
 
 
+time_stamp(start_time)
+print("STARTING TRAINING MODELS")
+
+
 # -------------------------------------------------MODEL SELECTION--------------------------------------
 # From: LogisticRegression, NaiveBayes, Random Forest, KNN, SVM with non-linear kernel
 
@@ -115,66 +125,122 @@ print(f'Y_TEST: \t\t{len(y_test)}')
 # todo provare i solver solver : str, {‘newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’}, default: ‘liblinear’.
 LogisticRegression_Classifier = LogisticRegression(solver='lbfgs', random_state=RANDOM_STATE) # put solver to silence the warning
 LogisticRegression_Classifier.fit(X_train, y_train)   # train the model
+save_pickle(LogisticRegression_Classifier, 'pickle_tmp/LogisticRegression_Classifier.pickle')
+
 '''
 # pesi delle classi {nomeclasse:peso; nomeclasse2:peso2, ...}
 LogisticRegressionModel = LogisticRegression(class_weight={1: 2 *len(good_queries)/len(bad_queries), 0: 1.0}) # class_weight='balanced')
 '''
 
+time_stamp(start_time)
+print("FINISHED LOGISTIC REGRESSION")
+
 # Naive Bayes
 MultinomialNB_Classifier = MultinomialNB()
 MultinomialNB_Classifier.fit(X_train, y_train)
+save_pickle(MultinomialNB_Classifier, 'pickle_tmp/MultinomialNB_Classifier.pickle')
 
-BernoulliNB_Classifier = BernoulliNB()       #T ODO probabilmente inutile...i dati non sono binari
+time_stamp(start_time)
+print("FINISHED MULTINOMIAL NAIVE BAYES")
+
+BernoulliNB_Classifier = BernoulliNB()       # TODO probabilmente inutile...i dati non sono binari
 BernoulliNB_Classifier.fit(X_train, y_train)
+save_pickle(BernoulliNB_Classifier, 'pickle_tmp/BernoulliNB_Classifier.pickle')
+
+time_stamp(start_time)
+print("FINISHED BERNOULLI NAIVE BAYES")
 
 ComplementNB_Classifier = ComplementNB()
 ComplementNB_Classifier.fit(X_train, y_train)
+save_pickle(ComplementNB_Classifier, 'pickle_tmp/ComplementNB_Classifier.pickle')
+
+time_stamp(start_time)
+print("FINISHED COMPLEMENT NAIVE BAYES")
 
 # KNN
 # KNeighborsClassifier_Classifier = KNeighborsClassifier(n_jobs=-1)      usare tutti i core sembra far rallentare
 # todo provare algorithm : {‘auto’, ‘ball_tree’, ‘kd_tree’, ‘brute’} se metto auto prende l'algo piu adatto in base ai parametri della fit
-KNeighborsClassifier_Classifier = KNeighborsClassifier(n_neighbors=5)        # lento e impreciso
-KNeighborsClassifier_Classifier.fit(X_train, y_train)
+KNeighbors_Classifier = KNeighborsClassifier(n_neighbors=5)        # slow and not precisen
+KNeighbors_Classifier.fit(X_train, y_train)
+save_pickle(KNeighbors_Classifier, 'pickle_tmp/KNeighbors_Classifier.pickle')
 
-RadiusNeighborsClassifier_Classifier = RadiusNeighborsClassifier(radius=1.0)
-RadiusNeighborsClassifier_Classifier.fit(X_train, y_train)              # todo error in validation
+time_stamp(start_time)
+print("FINISHED K NEIGHBORS")
 
-# todo Random forest and svc seems to not have fit method(), no warnings generated, but running the code result in infinite loop in that instruction
+RadiusNeighbors_Classifier = RadiusNeighborsClassifier(radius=1.0)
+RadiusNeighbors_Classifier.fit(X_train, y_train)              # todo error in validation
+save_pickle(RadiusNeighbors_Classifier, 'pickle_tmp/RadiusNeighbors_Classifier.pickle')
+
+time_stamp(start_time)
+print("FINISHED RADIUS NEIGHBORS")
 
 # RandomForest
-RandomForestClassifier_classifier = RandomForestClassifier(n_estimators=1000, criterion='gini', random_state=RANDOM_STATE)
+RandomForest_Classifier = RandomForestClassifier(n_estimators=100, criterion='gini', random_state=RANDOM_STATE)
+RandomForest_Classifier.fit(X_train, y_train)
+save_pickle(RandomForest_Classifier, 'pickle_tmp/RandomForest_Classifier.pickle')
 
+time_stamp(start_time)
+print("FINISHED RANDOM FOREST")
 
 
 # SVM
 # TODO provare altri kernel (# ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’)e cambiare C e gamma
 # usare i parametri gamma=grado per gli altri kernel e degree=grado della polinomiale
-SVC_Classifier = SVC(C=1.0, gamma='auto', cache_size=500, kernel='sigmoid', random_state=RANDOM_STATE)
+SVC_Classifier = SVC(C=1.0, gamma='auto', cache_size=500, kernel='sigmoid', random_state=RANDOM_STATE, probability=True)
 SVC_Classifier.fit(X_train, y_train)
-NuSVC_Classifier = NuSVC(nu=0.5, cache_size=500, kernel='sigmoid', random_state=RANDOM_STATE)
+save_pickle(SVC_Classifier, 'pickle_tmp/SVC_Classifier.pickle')
+
+time_stamp(start_time)
+print("FINISHED SVC")
+
+NuSVC_Classifier = NuSVC(nu=0.5, cache_size=500, kernel='sigmoid', random_state=RANDOM_STATE, probability=True) # messa probability altrimenti non funzionava la validazione
 NuSVC_Classifier.fit(X_train, y_train)
+save_pickle(NuSVC_Classifier, 'pickle_tmp/NuSVC_Classifier.pickle')
+
+time_stamp(start_time)
+print("FINISHED Nu-SVC")
 
 
 # Metrics of all models (use Validation dataset):
-# todo si potrebbe cambiare la funzione in modo che printi solo una stringa (riga) con i valori, poi fare ciclo per tutti i classificatori, in modo da avere una tabella di valori
 validate_model(LogisticRegression_Classifier, X_validation, y_validation, 'Logistic Regression')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR LOGISTIC REGRESSION")
 validate_model(MultinomialNB_Classifier, X_validation, y_validation, 'MultinomialNB')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR MULTINOMIAL NAIVE BAYES")
 validate_model(BernoulliNB_Classifier, X_validation, y_validation, 'BernoulliNB')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR BERNOULLI NAIVE BAYES")
 validate_model(ComplementNB_Classifier, X_validation, y_validation, 'ComplementNB')
-validate_model(KNeighborsClassifier_Classifier, X_validation, y_validation, 'KNeighborsClassifier')
-# validate_model(RadiusNeighborsClassifier_Classifier, X_validation, y_validation, 'RadiusNeighborsClassifier')
-validate_model(RandomForestClassifier_classifier, X_validation, y_validation, 'RandomForestClassifier')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR COMPLEMENT NAIVE BAYES")
+validate_model(KNeighbors_Classifier, X_validation, y_validation, 'KNeighborsClassifier')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR K NEIGHBORS")
+# validate_model(RadiusNeighbors_Classifier, X_validation, y_validation, 'RadiusNeighborsClassifier')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR RADIUS NEIGHBORS - NOT DONE DUE TO ERRORS")
+validate_model(RandomForest_Classifier, X_validation, y_validation, 'RandomForestClassifier')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR RANDOM FOREST")
 validate_model(SVC_Classifier, X_validation, y_validation, 'SVC')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR SVC")
 validate_model(NuSVC_Classifier, X_validation, y_validation, 'NuSVC')
+time_stamp(start_time)
+print("FINISHED VALIDATION FOR Nu-SVC")
 
 
 # Selected model:
 # Metrics of the selected model (use Test dataset):
 tmp_Classifier = LogisticRegression_Classifier # TODO remove and replace with selected classifier, also rename print
-validate_model(tmp_Classifier, X_test, y_test, 'TMP CLASSIFIER')
+validate_model(tmp_Classifier, X_test, y_test, 'BEST CLASSIFIER - TMP_NAME')
 
 # Pickle selected model:
 with open('pickle_tmp/queryStringClassifier.pickle', 'wb') as save_classifier_file:
     pickle.dump(tmp_Classifier, save_classifier_file)
+
+time_stamp(start_time)
+print("FINISHED SCRIPT")
 
 
